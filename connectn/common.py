8b17f56fc37e8acb3fe8185505c1f7392e4e2930
SudeshnaBora <sudeshnamani1@gmail.com>
2020-05-04 20:51:12 +0200

    Adding function definition and tests

--- connectn/common.py
+++ connectn/common.py
@@ -1,8 +1,7 @@
 from enum import Enum
-from typing import Optional
 import numpy as np
 
-BoardPiece = np.int8  # The data type (dtype) of the board
+BoardPiece = np.int8  # The data type of the board
 NO_PLAYER = BoardPiece(0)  # board[i, j] == NO_PLAYER where the position is empty
 PLAYER1 = BoardPiece(1)  # board[i, j] == PLAYER1 where player 1 has a piece
 PLAYER2 = BoardPiece(2)  # board[i, j] == PLAYER2 where player 2 has a piece
@@ -25,16 +24,76 @@ board[0, 0] = 2
 
 
 def pretty_print_board(input_board: np.ndarray) -> str:
-    return '\n'.join(['\t'.join([str(cell) for cell in row]) for row in input_board[::-1]])
+    return '\n'.join([str(row) for row in input_board[::-1]])
 
 
 def string_to_board(pp_board: str) -> np.ndarray:
     pass
 
 
-def apply_player_action(board: np.ndarray, action: PlayerAction, player: BoardPiece, copy: bool = False) -> np.ndarray:
-    pass
+def apply_player_action(d_board, action, player, copy=False) -> tuple:
+    placed = False
+    if copy:
+        copied_board = d_board
+    else:
+        copied_board = np.zeros((6, 7), dtype=BoardPiece)
+
+    for row in d_board:
+        if row[action] == NO_PLAYER:
+            d_board[row, action] = player
+            placed = True
+            break
+    if not placed:
+        raise Exception('Cannot place player in that particular position')
+
+    return copied_board, d_board
 
 
-def connected_four(board: np.ndarray, player: BoardPiece, last_action: Optional[PlayerAction] = None) -> bool:
-    pass
+def connected_four(d_board, player, last_action=None) -> bool:
+    rows = d_board.shape[0]
+    cols = d_board.shape[1]
+    if not (last_action in np.arange(rows)):
+        for i in range(rows):
+            for j in range(cols):
+                element = player
+                # check row
+                if j <= 3 and element == d_board[i, j + 1] and element == d_board[i, j + 2] and element == d_board[
+                    i, j + 3]:
+                    return True
+
+                # check column
+                if i <= 3 and element == d_board[i + 1, j] and element == d_board[i + 2, j] and element == d_board[
+                    i + 3, j]:
+                    return True
+
+                # right diagonal
+                if i <= 2 and j <= 3:
+                    if element == d_board[i + 1, j + 1] and element == d_board[i + 2, j + 2] and element == d_board[
+                    i + 3, j + 3]:
+                        return True
+
+                # left diagonal
+                if i <= 2 and j >= 3:
+                    if element == d_board[i + 1, j - 1] and element == d_board[i + 2, j - 2] and element == \
+                    d_board[i + 3, j - 3]:
+                        return True
+
+                return False
+
+
+def check_end_state(c4_board, player) -> GameState:
+    if connected_four(c4_board, player):
+        return GameState.IS_WIN
+    elif check_board_full(c4_board):
+        return GameState.IS_DRAW
+    else:
+        return GameState.STILL_PLAYING
+
+
+def check_board_full(c4_board) -> bool:
+    for row in range(6):
+        for col in range(7):
+            if c4_board[row, col] == 0:
+                return False
+
+    return True
--- connectn/tests/common_test.py
+++ connectn/tests/common_test.py
@@ -1,4 +1,6 @@
 import numpy as np
+from sympy.utilities import pytest
+
 from connectn.common import BoardPiece
 import connectn.common as cm
 
@@ -9,18 +11,18 @@ def test_pretty_print_board():
     pp_board = cm.pretty_print_board(board)
     test_board = np.zeros((6, 7), dtype=BoardPiece)
     test_board[0, 0] = cm.PLAYER1
-    str_test_board = '\n'.join(['\t'.join([str(cell) for cell in row]) for row in test_board[::-1]])
+    str_test_board = '\n'.join([str(row) for row in test_board[::-1]])
     assert pp_board.__eq__(str_test_board)
 
 
-def test_string_to_board():
+'''def test_string_to_board():
     board = cm.initialize_game_state()
-    pp_board = cm.pretty_print_board()
+    pp_board = cm.pretty_print_board(board)
     retrieved_board = cm.string_to_board(pp_board)
     assert isinstance(retrieved_board, np.ndarray)
     assert board.dtype == BoardPiece
     assert board.shape == (6, 7)
-    assert np.all(board == 0)
+    assert np.all(board == 0)'''
 
 
 def test_initialize_game_state():
@@ -30,3 +32,35 @@ def test_initialize_game_state():
     assert board.dtype == BoardPiece
     assert board.shape == (6, 7)
     assert np.all(board == 0)
+
+
+def test_apply_player_action():
+    board = cm.initialize_game_state()
+
+    copied_board, board = cm.apply_player_action(board, 3, cm.PLAYER1)
+    test_board = np.zeros((6, 7), dtype=BoardPiece)
+    test_board[0, 3] = cm.PLAYER1
+    assert np.all(test_board.__eq__(board))
+
+
+def test_connected_four():
+    board = cm.initialize_game_state()
+    board[0, 0] = cm.PLAYER1
+    board[1, 0] = cm.PLAYER1
+    board[2, 0] = cm.PLAYER1
+    board[3, 0] = cm.PLAYER1
+    assert cm.connected_four(board, cm.PLAYER1)
+
+
+def test_check_board_full():
+    board = cm.initialize_game_state()
+    assert ~cm.check_board_full(board)
+
+
+def test_check_end_state():
+    board = cm.initialize_game_state()
+    board[0, 0] = cm.PLAYER1
+    board[1, 0] = cm.PLAYER1
+    board[2, 0] = cm.PLAYER1
+    board[3, 0] = cm.PLAYER1
+    assert cm.GameState.IS_WIN.__eq__(cm.check_end_state(board, cm.PLAYER1))
